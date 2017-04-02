72a92c4be0dc53104ab5efebfa32f4d1b22658a7
koshigoe <koshigoeb@gmail.com>
2017-04-02 18:40:30 +0900

    4.5 JSON

---
+++ chapter4/issues/main.go
@@ -0,0 +1,23 @@
+// issues は検索語に一致した GitHub イシューの表を表示します。
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/koshigoe/the-go-programming-language/chapter4/github"
+)
+
+func main() {
+	result, err := github.SearchIssues(os.Args[1:])
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%d issues:\n", result.TotalCount)
+
+	for _, item := range result.Items {
+		fmt.Printf("#%-5d %9.9s %.55s\n",
+		item.Number, item.User.Login, item.Title)
+	}
+}
